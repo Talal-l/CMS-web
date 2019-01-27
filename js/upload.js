1445cf67a0ea92959b2a146fe9f66548369e3bf0
Ammar Alsarraf <ammary1996@gmail.com>
2019-01-27 13:01:22 +0300

    Add accepted emails to the upload page.

--- js/upload.js
+++ js/upload.js
@@ -3,23 +3,24 @@ $(document).ready(main);
 function main() {
 
     var acceptedMsgs = JSON.parse(localStorage.getItem('acceptedMsgs')) || [];
+    displayMsgs(acceptedMsgs);
 
 
     function displayMsgs(msgs) {
         console.log("acceptedmsg");
         console.log(acceptedMsgs);
-        var $emailCol = $("#emailData");
-        var $emailCard = $('#emailCard');
+        var $emailCol = $("#uploadData");
+        var $emailCard = $('#uploadCard');
         var $body = $("#baseBody");
 
         msgs.forEach((msg, i) => {
             var $emailCardClone = $emailCard.clone();
-            var emailCardId = 'emailCard-' + i;
+            var emailCardId = 'uploadCard-' + i;
             $emailCardClone.data(msg);
             $emailCardClone.attr('id', emailCardId);
-            $emailCardClone.find('#subject').text(msg.subject);
-            $emailCardClone.find('#date').text(msg.date);
-            $emailCardClone.find('#sender').text(msg.sender);
+            $emailCardClone.find('#requester').text(msg.subject);
+            $emailCardClone.find('#type').text(msg.date);
+            $emailCardClone.find('#requestedDate').text(msg.sender);
 
             // search in accepted msgs for the current msg 
             acceptedMsgs.some((acceptedMsg) => {
